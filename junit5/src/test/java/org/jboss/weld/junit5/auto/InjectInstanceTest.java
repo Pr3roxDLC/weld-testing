338bb949547a0d5523639f66a8a240266e969fa2
tim <pr3roxdlc.yt@web.de>
2024-03-05 21:30:48 +0100

    Adjust Test

--- junit5/src/test/java/org/jboss/weld/junit5/auto/InjectInstanceTest.java
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/InjectInstanceTest.java
@@ -6,21 +6,21 @@ import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 
+import org.jboss.weld.junit5.auto.classes.Bar;
+import org.jboss.weld.junit5.auto.classes.Baz;
+import org.jboss.weld.junit5.auto.classes.Foo;
 import org.junit.jupiter.api.Test;
 
 @EnableAutoWeld
 public class InjectInstanceTest {
-
-    @Dependent
-    static class Foo {
-    }
 
     @Inject
     Instance<Foo> fooInstance;
 
     @Test
     void test() {
-        assertNotNull(fooInstance.get());
+        assertNotNull(fooInstance.select(Bar.class).get());
+        assertNotNull(fooInstance.select(Baz.class).get());
     }
 
 }
---
+++ junit5/src/test/java/org/jboss/weld/junit5/auto/classes/Bar.java
@@ -0,0 +1,9 @@
+package org.jboss.weld.junit5.auto.classes;
+
+import jakarta.enterprise.context.Dependent;
+
+/**
+ * @author tim
+ */
+public class Bar extends Foo{
+}
